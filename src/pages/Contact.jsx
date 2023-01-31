469d50b909fd7738c18c6cbf8851d0030cd3a537
Austin Friesorger <aafrieso@svsu.edu>
2023-01-31 15:23:03 -0500

    update contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,11 @@
 function Contact() {
   return (
+    <>
       <h1>Contact</h1>
+      <p>Email: austinafrieso@outlook.com</p>
+      <p>LinkedIn: https://www.linkedin.com/in/austin-friesorger-7a033316b/</p>
+      <p>GitHub: https://github.com/aafrieso</p>
+    </>
   )
 }
-
 export default Contact
