16ba40cffb61b5d4b3852ef2f022374c5f09c67a
Austin Friesorger <aafrieso@svsu.edu>
2023-01-31 21:32:52 -0500

    add img to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,11 @@
+import '../App.css';
+
 function Home() {
   return (
     <>
       <h2>Austin Friesorger</h2>
       <h2>Aspiring Software Engineer</h2>
+      <img src="/images/IMG_8883.jpg" alt=""/>
     </>
   )
 }
